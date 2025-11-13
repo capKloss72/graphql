5a51d70d7e77d15da47416364b1c535227df1684
capKloss72 <tym.zon@gmail.com>
2025-11-13 05:26:26 +0000

    Adding inital graphql setup

---
+++ server.js
@@ -0,0 +1,22 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';  
+
+const typeDefs = `#graphql
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => 'Hello, world!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});  
+
+const info = await startStandaloneServer(server, { listen: { port: 9000 } });
+console.log(`Server is running at: ${info.url}`);
